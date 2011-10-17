272f09bf6f1aa0e672222e7596a109ef00709739
Thomas d'Otreppe <tdotreppe@aircrack-ng.org>
2011-10-17 02:35:02 +0000

    Plugins: Moved common plugin stuff into another header and includes it in the frame plugin header.

---
+++ server/plugin/generic_plugin_header.h
@@ -0,0 +1,29 @@
+/*
+ *     License: BSD/GPLv2
+ *      Author: Thomas d'Otreppe de Bouvette
+ */
+
+#ifndef GENERIC_PLUGIN_HEADER_H_
+#define GENERIC_PLUGIN_HEADER_H_
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+void * init_plugin(char * config_line, int version);
+void free_memory_and_unload(void * data);
+char plugin_type(void);
+int min_supported_version();
+int max_supported_version();
+char * init_text(void * config);
+
+#define PLUGIN_TYPE_FRAME					'F'
+#define PLUGIN_TYPE_DATABASE_CONNECTION		'D'
+#define PLUGIN_TYPE_ALERT					'A'
+#define PLUGIN_TYPE_LOGGING					'L'
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif //GENERIC_PLUGIN_HEADER_H_
