ac469624038412a735071526da263e3b1a572a89
misterx <misterx@08f09bf2-93fc-4daa-afec-f37e8f70a565>
2011-10-19 02:22:18 +0000

    Plugin: Added Fragmentation detection.

--- trunk/server/plugin/frag_detection.c
+++ trunk/server/plugin/frag_detection.c
@@ -22,33 +22,63 @@
 #include <string.h>
 #include "frame_plugin_header.h"
 
-// Need a struct similar to replay + implement fragment_nr + more_frag in frame
+#define NB_ADDRESSES 4
+
+struct frag_attack {
+	unsigned int nb_frames; // TODO: Handle more than one frame and several attacks at the same time
+	unsigned char **addresses;
+	int nb_mac;
+};
 
 void * init_plugin(char * config_line, int version)
 {
-	unsigned int * nb_frames = (unsigned int*)malloc(sizeof(unsigned int));
-	*nb_frames = 1; // By default 1 frame is enough
+	int i;
+	struct frag_attack * fa = (struct frag_attack *)malloc(sizeof(struct frag_attack));
+	fa->nb_frames = 1; // By default 1 frame is enough (frag should never happen on any network
 
-	if (config_line) {
-		*nb_frames = atoi(config_line);
-		if (*nb_frames == 0) {
-			*nb_frames = 1;
-		}
+	// Allocate memory at startup
+	fa->addresses = (unsigned char **)malloc(sizeof(unsigned char *) * NB_ADDRESSES);
+	for (i = 0; i < NB_ADDRESSES; i++) {
+		fa->addresses[i] = (unsigned char *)malloc(sizeof(unsigned char) * MAC_LEN);
 	}
 
-	return nb_frames;
+	fa->nb_mac = 0;
+
+	/*
+	if (config_line) {
+
+		fa->nb_frames = atoi(config_line);
+		if (fa->nb_frames == 0) {
+			fa->nb_frames = 1;
+		}
+	}
+	*/
+
+	return fa;
 }
 
 void free_memory_and_unload(void * data)
 {
-	if (data) {
-		free(data);
+	int i;
+	struct frag_attack * fa;
+
+	if (!data) {
+		return;
 	}
+
+	fa = (struct frag_attack *)data;
+
+	for (i = 0; i < NB_ADDRESSES; i++) {
+		free(fa->addresses[i]);
+	}
+	free(fa->addresses);
+
+	free(fa);
 }
 
 char plugin_type(void)
 {
-	return 'F';
+	return PLUGIN_TYPE_FRAME;
 }
 
 int min_supported_version()
@@ -63,10 +93,11 @@ int max_supported_version()
 
 char * init_text(void * config)
 {
-	char * ret = (char *)calloc(1, 100 * sizeof(char));
+	struct frag_attack * fa;
+	char * ret = (char *)calloc(1, 80 * sizeof(char));
 	if (config) {
-		sprintf(ret, "Fragmentation attack detection (with %u frames) v1.0",
-				*((unsigned int *)config));
+		fa = (struct frag_attack *)config;
+		sprintf(ret, "Fragmentation attack detection (with %u frames) v1.0", fa->nb_frames);
 	} else {
 		strcpy(ret, "Fragmentation attack detection v1.0");
 	}
@@ -76,7 +107,8 @@ char * init_text(void * config)
 
 int static_frame_type()
 {
-	// It should only be data frames with data but there can be an idiot out there to try another kind of frame
+	// It should only be data frames with data but there can be
+	// an idiot out there to try it on control/management frames too (more frag bit on control)
 	return -1;
 }
 
@@ -103,7 +135,7 @@ int require_packet_parsed()
 
 int can_use_frame(struct pcap_packet * packet, void * config)
 {
-	if (packet == NULL || config == NULL) {
+	if (packet == NULL || packet->info == NULL || config == NULL) {
 		return 0;
 	}
 
@@ -116,13 +148,18 @@ int analyze(struct pcap_packet * packet, void * config)
 		return 0;
 	}
 
-	//packet->info->fragmentation
-	return 0;
+	return packet->info->fragment_nr || packet->info->more_frag;
 }
 
 int nb_frames_before_analyzing(void * config)
 {
-	return (config) ? ((int) (*((unsigned int *)config))) : 1;
+	struct frag_attack * fa;
+	if (config) {
+		fa = (struct frag_attack *)config;
+		return fa->nb_frames;
+	}
+
+	return 1;
 }
 
 int time_ms_before_analyzing(void * config)
@@ -133,24 +170,103 @@ int time_ms_before_analyzing(void * config)
 
 int is_attacked(struct pcap_packet * packet_list, void * config)
 {
-	return 0;
+	int counter;
+	struct frag_attack * fa;
+	int ret = analyze(packet_list, config);
+	if (packet_list == NULL || packet_list->info == NULL || config == NULL || ret == 0) {
+		return 0;
+	}
+
+	// TODO: Make sure we can handle multiple attacks at the same time when more than 1 frame is required (the amount of frame PER attack).
+	//       Do a chained list and same logic like another plugin.
+
+	// Keep attacker info in the structure
+	counter = 0;
+	fa = (struct frag_attack *)config;
+
+	// Copy MACs
+	if (packet_list->info->address1) {
+		memcpy(fa->addresses[counter++], packet_list->info->address1, MAC_LEN);
+	}
+	if (packet_list->info->address2) {
+		memcpy(fa->addresses[counter++], packet_list->info->address2, MAC_LEN);
+	}
+	if (packet_list->info->address3) {
+		memcpy(fa->addresses[counter++], packet_list->info->address3, MAC_LEN);
+	}
+	if (packet_list->info->address4) {
+		memcpy(fa->addresses[counter++], packet_list->info->address4, MAC_LEN);
+	}
+
+	fa->nb_mac = counter;
+
+	return ret;
 }
 
 char * attack_details(void * config)
 {
-	return NULL;
+	int i;
+	struct frag_attack * fa;
+	char * ret = NULL;
+
+	if (config == NULL) {
+		return NULL;
+	}
+
+	fa = (struct frag_attack *)config;
+	ret = (char *)calloc(1, 50 + (fa->nb_mac * 3 * MAC_LEN));
+
+	strcpy(ret, "ALERT - Fragmentation attack in progress");
+	if (fa->nb_mac) {
+		// TODO: Only the calling thread should reveal the mac since the plugin doesn't do any check on it.
+		strcat(ret, ". MAC Addresses involved:");
+		for (i = 0; i < fa->nb_mac; i++) {
+			sprintf(ret + strlen(ret), " %02X:%02X:%02X:%02X:%02X:%02X",
+									fa->addresses[i][0], fa->addresses[i][1], fa->addresses[i][2],
+									fa->addresses[i][3], fa->addresses[i][4], fa->addresses[i][5]);
+		}
+	}
+
+	return ret;
 }
 
 unsigned char ** get_attacker_macs(void * config, int * nb_mac, int * deauth)
 {
+	int i;
+	int counter;
+	unsigned char ** macs;
+	struct frag_attack * fa;
+	if (config == NULL) {
+		return NULL;
+	}
+
+	fa = (struct frag_attack *)config;
+
 	if (deauth) {
 		*deauth = 1;
 	}
 
-	return NULL;
+	if (nb_mac) {
+		*nb_mac = fa->nb_mac;
+	}
+
+	// Copy the mac addresses
+	macs = (unsigned char **)malloc(sizeof(unsigned char *) * (fa->nb_mac));
+	for (i = 0; i < fa->nb_mac; i++) {
+		COPY_MAC(fa->addresses[i], macs, counter)
+	}
+
+	return fa->addresses;
 }
 
 void clear_attack(void * config)
 {
-	if (config) { }
+	struct frag_attack * fa;
+	if (config) {
+		fa = (struct frag_attack *)config;
+		// Do not free, memory (since we know how much we allocated at startup, we know what to free at exit).
+		fa->nb_mac = 0;
+	}
 }
+
+#undef NB_ADDRESSES
