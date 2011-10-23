4fe5933b354c00fb638498b62727bbf046c9b3a2
misterx <misterx@08f09bf2-93fc-4daa-afec-f37e8f70a565>
2011-10-23 18:44:49 +0000

    Common: Added missing includes in deamonize.c.

--- trunk/common/deamonize.c
+++ trunk/common/deamonize.c
@@ -19,6 +19,8 @@
  *      Author: Thomas d'Otreppe de Bouvette
  */
 #include <stdlib.h>
+#include <stdio.h>
+#include <unistd.h>
 #include <sys/types.h>
 #include <sys/stat.h>
 #include "deamonize.h"
